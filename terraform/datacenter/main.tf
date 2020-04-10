ec1f8c04f50840e62eb9aead3175a6b2a4f105cc
Alex Harness <9669770+mocofound@users.noreply.github.com>
2020-04-10 17:39:42 -0500

    Update main.tf

--- terraform/datacenter/main.tf
+++ terraform/datacenter/main.tf
@@ -25,6 +25,17 @@ module "vpc" {
   enable_nat_gateway   = true
   single_nat_gateway   = true
   enable_dns_hostnames = true
+  
+    public_subnet_tags = {
+    "kubernetes.io/cluster/${local.cluster_name}" = "shared"
+    "kubernetes.io/role/elb"                      = "1"
+  }
+
+  private_subnet_tags = {
+    "kubernetes.io/cluster/${local.cluster_name}" = "shared"
+    "kubernetes.io/role/internal-elb"             = "1"
+  }
+
 
 }
 
