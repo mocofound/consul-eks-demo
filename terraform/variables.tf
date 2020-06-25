392db93b7def17e8c08421e46918bf345c8a1c85
Alex Harness <9669770+mocofound@users.noreply.github.com>
2020-06-25 14:56:03 -0500

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -9,7 +9,9 @@ variable "environment" {
   default= "dev"
 }
 
-variable "ssh_key_name" {}
+variable "ssh_key_name" {
+  description = "SSH Key must be present in all AWS regions being deployed"
+  }
 
 variable "worker_instance_size" {
   default = "m5a.xlarge"
